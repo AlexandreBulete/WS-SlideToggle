d58ccfc3a8fb23fb6e8b522eac4cdf1d0afd7d5e
Alexandre Buleté <bulete.alexandre@gmail.com>
2019-11-18 07:29:03 +0100

    style: demo

--- demo/index.php
+++ demo/index.php
@@ -6,19 +6,42 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Demo WS SlideToggle</title>
     <script src="slideToggle.js"></script>
+    <style lang="css">
+        #wrap-btn {
+            margin: 0;
+            background: #136e59;
+            color: white;
+            padding: 5px 15px;
+            border-top-left-radius: 5px;
+            border-top-right-radius: 5px;
+        }
+        #wrap-btn:hover {
+            cursor: pointer;
+        }
+        #wrap-content {
+            background-color: #f6f6f6;
+        }
+        #wrap-content p {
+            /* border: 1px solid lightgrey; */
+            padding: 5px 15px;
+            margin: 0;
+        }
+    </style>
 </head>
 <body>
     <h1>Demo WS-SlideToggle</h1>
-    <button id="elem">Click here</button>
+    <p id="wrap-btn">Click here</p>
     <div id="wrap-content">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod vel ullam ab voluptatum velit natus nihil. Omnis culpa mollitia, dolorem enim totam itaque architecto eos labore soluta! Quam, eum illo?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod vel ullam ab voluptatum velit natus nihil. Omnis culpa mollitia, dolorem enim totam itaque architecto eos labore soluta! Quam, eum illo?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod vel ullam ab voluptatum velit natus nihil. Omnis culpa mollitia, dolorem enim totam itaque architecto eos labore soluta! Quam, eum illo?</p>
+        <p>ligne 1</p>
+        <p>ligne 2</p>
+        <p>ligne 3</p>
+        <p>ligne 4</p>
+        <p>ligne 5</p>
     </div>
     
     <script>
         slideToggle(
-            document.getElementById('elem'), 
+            document.getElementById('wrap-btn'), 
             document.getElementById('wrap-content'),
             100
         );
